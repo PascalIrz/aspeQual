4bd6be2b8460ebf3870098257dc99a78d7fb85c8
PascalIrz <pascal.irz@gmail.com>
2021-12-03 15:30:13 +0100

    détails qta_tests_1lot_1var ()

--- R/qta_tests_1lot_1var.R
+++ R/qta_tests_1lot_1var.R
@@ -5,7 +5,7 @@
 #'
 #' @param df Dataframe de données avec une variable "lop_id" et le nom de la variable choisie
 #'     à représenter.
-#' @param id_lot Entier. Numéro du lot.
+#' @param lop_id Entier. Numéro du lot.
 #' @param variable Caractère. Nom de la variable à représenter, entre guillemets.
 #'
 #' @return Un dataframe avec une ligne contenant les résultats des tests.
@@ -15,12 +15,15 @@
 #' @importFrom stats ks.test t.test var.test
 #'
 #' @examples
-qta_tests_1lot_1var <- function(df, id_lot, variable)
+#' \dontrun{
+#' tests <- qta_tests_1lot_1var(df = df, lop_id = 3156360, variable = "mei_taille")
+#' }
+qta_tests_1lot_1var <- function(df, lop_id, variable)
 
 {
 
   lot_data <- df %>%
-    filter(lop_id == id_lot)
+    filter(lop_id == lop_id)
 
   t <- lot_data %>%
     filter(mei_mesure_reelle == "t") %>%
@@ -45,7 +48,7 @@ qta_tests_1lot_1var <- function(df, id_lot, variable)
 
   eff_non_mesure <- nrow(lot_data) - eff_mesure
 
-  lop_id <- id_lot
+  lop_id <- lop_id
 
   station = lot_data$pop_libelle[1]
 
